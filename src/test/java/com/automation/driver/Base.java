04ee944e69d697325ea6465d9b4504f9e8982ae3
Sairam Sivaswami <ss_sairam@yahoo.com>
2020-04-28 06:41:44 +1200

    added browser initiation from mvn

--- src/test/java/com/automation/driver/Base.java
+++ src/test/java/com/automation/driver/Base.java
@@ -1,7 +1,10 @@
 package com.automation.driver;
 
 
+import com.automation.steps.HomePageAssertions;
 import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -15,13 +18,21 @@ import java.util.concurrent.TimeUnit;
 public class Base {
     public static WebDriver driver;
     public Properties prop = new Properties();
+    String browser;
+    public static Logger log = LogManager.getLogger(Base.class.getName());
 
     public WebDriver initialiseDriver() throws IOException {
         InputStream is = getClass()
                 .getClassLoader().getResourceAsStream("application.properties");
         //  FileInputStream is = new FileInputStream("E:\\Project\\automation-sample\\src\\main\\java\\com\\automation\\application.properties");
         prop.load(is);
-        String browser = prop.getProperty("browser").toLowerCase();
+        if(System.getProperty("browser")==null) {
+             browser= prop.getProperty("browser").toLowerCase();
+        } else{
+            browser = System.getProperty("browser");
+        }
+
+        log.info("Browser Selected::" + browser);
         if (browser.equals("chrome")) {
             System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sairam\\Downloads\\chromedriver_win32\\chromedriver.exe");
             driver = new ChromeDriver();
@@ -29,7 +40,7 @@ public class Base {
             System.setProperty("webdriver.gecko.driver", "C:\\Users\\Sairam\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe");
             driver = new FirefoxDriver();
         } else {
-            System.out.println("No browser defined");
+           log.info("No browser defined");
         }
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         return driver;
